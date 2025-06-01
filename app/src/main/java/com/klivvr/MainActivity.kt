aa41731189934a255582ad7d6546ca725df2dcb9
Ahmed Elmenshawie <ahmed.elmenshawie@gmail.com>
2025-06-02 00:13:06 +0300

    splash screen

--- app/src/main/java/com/klivvr/MainActivity.kt
+++ app/src/main/java/com/klivvr/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.mutableStateOf
 import com.klivvr.core.designSystem.KlivvrTheme
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
@@ -14,15 +20,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        installSplashScreen().apply {
-            setKeepOnScreenCondition {
-                true
-            }
+
+        val splashScreen = installSplashScreen()
+        var keepSplashOnScreen by mutableStateOf(true)
+
+        splashScreen.setKeepOnScreenCondition {
+            keepSplashOnScreen
         }
+
         setContent {
             KlivvrTheme {
                 KlivvrNavHost()
             }
+        }
+
+        lifecycleScope.launch {
+            delay(2000)
+            keepSplashOnScreen = false
         }
     }
 }
